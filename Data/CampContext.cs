bad064a7e04fd0265ab05fb117d8303f3cad51a9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-08-06 16:29:46 -0500

    Added controller

---
+++ Data/CampContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CampTracker.Models;
+
+namespace CampTracker.Data
+{
+    public class CampContext : DbContext
+    {
+        public CampContext (DbContextOptions<CampContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CampTracker.Models.Camp> Camp { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CampTracker.Data;
 
 namespace CampTracker
 {
@@ -26,6 +28,9 @@ namespace CampTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<CampContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CampContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
